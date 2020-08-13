a59032f50e50c6c0a8b92dd51e30fe7e3f05038a
LynnL4 <lht856@foxmail.com>
2020-08-13 15:21:41 +0800

    an initialization Seeed_Arduino_erpcUnified project

---
+++ src/Seeed_erpcUnified.h
@@ -0,0 +1,11 @@
+  
+/*
+    The MIT License (MIT)
+    Copyright (C) 2020  Seeed Technology Co.,Ltd.
+*/
+#ifndef _SEEED_ERPCUNIFIED_H__
+#define _SEED_ERPCUNIFIED_H__
+
+#include <Arduino.h>
+
+#endif//_SEEED_ERPCUNIFIED_H__
