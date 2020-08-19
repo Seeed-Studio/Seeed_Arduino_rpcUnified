17a61c4ee08c65085e1ec754af4f7bd2e3363e80
LynnL4 <lht856@foxmail.com>
2020-08-19 17:04:20 +0800

    add rpc gap service client hal api.

--- src/rtl_ble/rpc_ble_api_hal.cpp
+++ src/rtl_ble/rpc_ble_api_hal.cpp
@@ -1,120 +1,43 @@
 #include <stdlib.h>
+#include <string.h>
 #include "rpc_ble_api_hal.h"
 #include "rpc_ble_api.h"
 #include "gap_adv.h"
+#include "erpc_port.h"
+
+//! @brief Function to free space allocated inside struct binary_t
+static void free_binary_t_struct(binary_t *data);
+
+// Free space allocated inside struct binary_t function implementation
+static void free_binary_t_struct(binary_t *data)
+{
+    if (data->data)
+    {
+        erpc_free(data->data);
+    }
+}
 
 extern P_FUN_LE_APP_CB _ble_gap_callback;
-
 
 void le_register_app_cb(P_FUN_LE_APP_CB ble_gap_callback)
 {
     _ble_gap_callback = ble_gap_callback;
 }
 
-T_GAP_CAUSE le_adv_set_param(T_LE_ADV_PARAM_TYPE param, uint8_t len, void *p_value)
+//! @name rpc_gap
+//@{
+T_GAP_CAUSE gap_set_param(T_GAP_PARAM_TYPE param, uint8_t len, void *p_value)
 {
-    binary_t value;
-    value.dataLength = len;
-    value.data = (uint8_t *)p_value;
-    return (T_GAP_CAUSE)rpc_le_adv_set_param((RPC_T_LE_ADV_PARAM_TYPE)param, &value);
+    RPC_FUN_GAP_SET_PARAM(gap_set_param, T_GAP_PARAM_TYPE);
 }
 
-T_GAP_CAUSE le_adv_get_param(T_LE_ADV_PARAM_TYPE param, void *p_value)
+T_GAP_CAUSE gap_get_param(T_GAP_PARAM_TYPE param, void *p_value)
 {
-    binary_t value;
-    value.dataLength = 0;
-    value.data = (uint8_t *)p_value;
-    return (T_GAP_CAUSE)rpc_le_adv_get_param((RPC_T_LE_ADV_PARAM_TYPE)param, &value);
+    RPC_FUN_GAP_GET_PARAM(gap_get_param, T_GAP_PARAM_TYPE);
 }
 
-T_GAP_CAUSE le_adv_start(void)
+T_GAP_CAUSE gap_set_pairable_mode(void)
 {
-    return (T_GAP_CAUSE)rpc_le_adv_start();
+    RPC_FUN_GAP_VOID(gap_set_pairable_mode);
 }
-
-T_GAP_CAUSE le_adv_stop(void)
-{
-    return (T_GAP_CAUSE)le_adv_stop();
-}
-
-T_GAP_CAUSE le_adv_update_param(void)
-{
-    return (T_GAP_CAUSE)le_adv_update_param();
-}
-bool le_gap_init(uint8_t link_num)
-{
-    return rpc_le_gap_init(link_num);
-}
-
-void le_gap_msg_info_way(bool use_msg)
-{
-    return le_gap_msg_info_way(use_msg);
-}
-
-uint8_t le_get_max_link_num(void)
-{
-    return rpc_le_get_max_link_num();
-}
-
-T_GAP_CAUSE le_set_gap_param(T_GAP_LE_PARAM_TYPE param, uint8_t len, void *p_value)
-{
-    binary_t value;
-    value.dataLength = len;
-    value.data = (uint8_t *)p_value;
-    return (T_GAP_CAUSE)rpc_le_set_gap_param((RPC_T_GAP_LE_PARAM_TYPE)param, &value);
-}
-
-T_GAP_CAUSE le_get_gap_param(T_GAP_LE_PARAM_TYPE param, void *p_value)
-{
-    binary_t value;
-    value.dataLength = 0;
-    value.data = (uint8_t *)p_value;
-    return (T_GAP_CAUSE)rpc_le_set_gap_param((RPC_T_GAP_LE_PARAM_TYPE)param, &value);
-}
-T_GAP_CAUSE le_modify_white_list(T_GAP_WHITE_LIST_OP operation, uint8_t *bd_addr,
-                                 T_GAP_REMOTE_ADDR_TYPE bd_type)
-{
-    return (T_GAP_CAUSE)rpc_le_modify_white_list((RPC_T_GAP_WHITE_LIST_OP)operation, bd_addr, (RPC_T_GAP_REMOTE_ADDR_TYPE)bd_type);
-}
-T_GAP_CAUSE le_gen_rand_addr(T_GAP_RAND_ADDR_TYPE rand_addr_type, uint8_t *random_bd)
-{
-    return (T_GAP_CAUSE)rpc_le_gen_rand_addr((RPC_T_GAP_RAND_ADDR_TYPE)rand_addr_type, random_bd);
-}
-
-T_GAP_CAUSE le_set_rand_addr(uint8_t *random_bd)
-{
-    return (T_GAP_CAUSE)rpc_le_set_rand_addr(random_bd);
-}
-
-
-
-T_GAP_CAUSE le_scan_set_param(T_LE_SCAN_PARAM_TYPE param, uint8_t len, void *p_value)
-{
-    binary_t value;
-    value.dataLength = len;
-    value.data = (uint8_t *)p_value;
-    return (T_GAP_CAUSE)rpc_le_scan_set_param((RPC_T_LE_SCAN_PARAM_TYPE)param, &value);
-}
-
-T_GAP_CAUSE le_scan_get_param(T_LE_SCAN_PARAM_TYPE param, void *p_value)
-{
-    binary_t value;
-    value.dataLength = 0;
-    value.data = (uint8_t *)p_value;
-    return (T_GAP_CAUSE)rpc_le_scan_get_param((RPC_T_LE_SCAN_PARAM_TYPE)param, &value);
-}
-
-T_GAP_CAUSE le_scan_start(void)
-{
-    return (T_GAP_CAUSE)rpc_le_scan_start();
-}
-
-T_GAP_CAUSE le_scan_stop(void)
-{
-    return (T_GAP_CAUSE)rpc_le_scan_stop();
-}
-
-// bool le_scan_info_filter(bool enable, uint8_t offset, uint8_t len, uint8_t *p_filter)
-// {
-   
-// }
+//@}
--- src/rtl_ble/rpc_ble_api_hal.h
+++ src/rtl_ble/rpc_ble_api_hal.h
@@ -13,4 +13,26 @@
 #include "gap_scan.h"
 #include "gap_storage_le.h"
 
+#define RPC_FUN_GAP_SET_PARAM(FUN, PARAM_TYPE)                     \
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;                           \
+    binary_t value;                                                \
+    value.dataLength = len;                                        \
+    value.data = (uint8_t *)p_value;                               \
+    ret = (T_GAP_CAUSE)rpc_##FUN((RPC_##PARAM_TYPE)param, &value); \
+    return ret
+
+#define RPC_FUN_GAP_GET_PARAM(FUN, PARAM_TYPE)                     \
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;                           \
+    binary_t value;                                                \
+    ret = (T_GAP_CAUSE)rpc_##FUN((RPC_##PARAM_TYPE)param, &value); \
+    if (GAP_SUCCESS == ret)                                        \
+        memcpy(p_value, value.data, value.dataLength);             \
+    erpc_free(value.data);                                         \
+    return ret
+
+#define RPC_FUN_GAP_VOID(FUN)            \
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS; \
+    ret = (T_GAP_CAUSE)FUN();            \
+    return ret
+
 void le_register_app_cb(P_FUN_LE_APP_CB gap_callback);
