76e5b596f60798268f2bf7be266b9e4dee3c9673
LynnL4 <lht856@foxmail.com>
2020-08-19 17:27:11 +0800

    add rpc gap le service client hal api.

--- src/rtl_ble/rpc_ble_api_hal.cpp
+++ src/rtl_ble/rpc_ble_api_hal.cpp
@@ -17,13 +17,6 @@ static void free_binary_t_struct(binary_t *data)
     }
 }
 
-extern P_FUN_LE_APP_CB _ble_gap_callback;
-
-void le_register_app_cb(P_FUN_LE_APP_CB ble_gap_callback)
-{
-    _ble_gap_callback = ble_gap_callback;
-}
-
 //! @name rpc_gap
 //@{
 T_GAP_CAUSE gap_set_param(T_GAP_PARAM_TYPE param, uint8_t len, void *p_value)
@@ -116,6 +109,73 @@ T_GAP_CAUSE le_bond_get_sec_level(uint8_t conn_id, T_GAP_SEC_LEVEL *p_type)
 {
     T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
     ret = (T_GAP_CAUSE)rpc_le_bond_get_sec_level(conn_id, (RPC_T_GAP_SEC_LEVEL *)p_type);
+    return ret;
+}
+//@}
+
+//! @name rpc_gap_le
+//@{
+bool le_gap_init(uint8_t link_num)
+{
+    return rpc_le_gap_init(link_num);
+}
+void le_gap_msg_info_way(bool use_msg)
+{
+    return rpc_le_gap_msg_info_way(use_msg);
+}
+uint8_t le_get_max_link_num(void)
+{
+    return le_get_max_link_num();
+}
+extern P_FUN_LE_APP_CB _ble_gap_callback;
+void le_register_app_cb(P_FUN_LE_APP_CB ble_gap_callback)
+{
+    _ble_gap_callback = ble_gap_callback;
+}
+
+T_GAP_CAUSE le_set_gap_param(T_GAP_LE_PARAM_TYPE param, uint8_t len, void *p_value)
+{
+    RPC_FUN_GAP_GET_PARAM(le_set_gap_param, T_GAP_LE_PARAM_TYPE);
+}
+T_GAP_CAUSE le_get_gap_param(T_GAP_LE_PARAM_TYPE param, void *p_value)
+{
+    RPC_FUN_GAP_GET_PARAM(le_get_gap_param, T_GAP_LE_PARAM_TYPE);
+}
+T_GAP_CAUSE le_modify_white_list(T_GAP_WHITE_LIST_OP operation, uint8_t *bd_addr,
+                                 T_GAP_REMOTE_ADDR_TYPE bd_type)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_modify_white_list((RPC_T_GAP_WHITE_LIST_OP)operation, bd_addr, (RPC_T_GAP_REMOTE_ADDR_TYPE)bd_type);
+    return ret;
+}
+T_GAP_CAUSE le_gen_rand_addr(T_GAP_RAND_ADDR_TYPE rand_addr_type, uint8_t *random_bd)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_gen_rand_addr((RPC_T_GAP_RAND_ADDR_TYPE)rand_addr_type, random_bd);
+    return ret;
+}
+T_GAP_CAUSE le_set_rand_addr(uint8_t *random_bd)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_set_rand_addr(random_bd);
+    return ret;
+}
+T_GAP_CAUSE le_cfg_local_identity_address(uint8_t *addr, T_GAP_IDENT_ADDR_TYPE type)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_cfg_local_identity_address(addr, (RPC_T_GAP_IDENT_ADDR_TYPE)type);
+    return ret;
+}
+T_GAP_CAUSE le_set_host_chann_classif(uint8_t *p_channel_map)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_set_host_chann_classif(*p_channel_map);
+    return ret;
+}
+T_GAP_CAUSE le_write_default_data_len(uint16_t tx_octets, uint16_t tx_time)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_write_default_data_len(tx_octets, tx_time);
     return ret;
 }
 //@}
