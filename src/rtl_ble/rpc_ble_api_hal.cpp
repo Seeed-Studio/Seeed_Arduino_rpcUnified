7f8ec2190c4440fe1ff3d8ef68ee77fed5d268fe
LynnL4 <lht856@foxmail.com>
2020-08-19 17:05:34 +0800

    add rpc gap bone service client hal api.

--- src/rtl_ble/rpc_ble_api_hal.cpp
+++ src/rtl_ble/rpc_ble_api_hal.cpp
@@ -41,3 +41,81 @@ T_GAP_CAUSE gap_set_pairable_mode(void)
     RPC_FUN_GAP_VOID(gap_set_pairable_mode);
 }
 //@}
+//! @name rpc_gap_bone
+//@{
+T_GAP_CAUSE le_bond_set_param(T_LE_BOND_PARAM_TYPE param, uint8_t len, void *p_value)
+{
+    RPC_FUN_GAP_SET_PARAM(le_bond_set_param, T_LE_BOND_PARAM_TYPE);
+}
+T_GAP_CAUSE le_bond_pair(uint8_t conn_id)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_pair(conn_id);
+    return ret;
+}
+T_GAP_CAUSE le_bond_get_display_key(uint8_t conn_id, uint32_t *p_key)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_get_display_key(conn_id, p_key);
+    return ret;
+}
+T_GAP_CAUSE le_bond_passkey_input_confirm(uint8_t conn_id, uint32_t passcode,
+                                          T_GAP_CFM_CAUSE cause)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_passkey_input_confirm(conn_id, passcode, (RPC_T_GAP_CFM_CAUSE)cause);
+    return ret;
+}
+T_GAP_CAUSE le_bond_oob_input_confirm(uint8_t conn_id, T_GAP_CFM_CAUSE cause)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_oob_input_confirm(conn_id, (RPC_T_GAP_CFM_CAUSE)cause);
+    return ret;
+}
+T_GAP_CAUSE le_bond_just_work_confirm(uint8_t conn_id, T_GAP_CFM_CAUSE cause)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_just_work_confirm(conn_id, (RPC_T_GAP_CFM_CAUSE)cause);
+    return ret;
+}
+T_GAP_CAUSE le_bond_passkey_display_confirm(uint8_t conn_id, T_GAP_CFM_CAUSE cause)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_passkey_display_confirm(conn_id, (RPC_T_GAP_CFM_CAUSE)cause);
+    return ret;
+}
+T_GAP_CAUSE le_bond_user_confirm(uint8_t conn_id, T_GAP_CFM_CAUSE cause)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_user_confirm(conn_id, (RPC_T_GAP_CFM_CAUSE)cause);
+    return ret;
+}
+T_GAP_CAUSE le_bond_cfg_local_key_distribute(uint8_t init_dist, uint8_t rsp_dist)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_cfg_local_key_distribute(init_dist, rsp_dist);
+    return ret;
+}
+void le_bond_clear_all_keys(void)
+{
+    return rpc_le_bond_clear_all_keys();
+}
+T_GAP_CAUSE le_bond_delete_by_idx(uint8_t idx)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_delete_by_idx(idx);
+    return ret;
+}
+T_GAP_CAUSE le_bond_delete_by_bd(uint8_t *bd_addr, T_GAP_REMOTE_ADDR_TYPE bd_type)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_delete_by_bd(bd_addr, (RPC_T_GAP_REMOTE_ADDR_TYPE)bd_type);
+    return ret;
+}
+T_GAP_CAUSE le_bond_get_sec_level(uint8_t conn_id, T_GAP_SEC_LEVEL *p_type)
+{
+    T_GAP_CAUSE ret = GAP_CAUSE_SUCCESS;
+    ret = (T_GAP_CAUSE)rpc_le_bond_get_sec_level(conn_id, (RPC_T_GAP_SEC_LEVEL *)p_type);
+    return ret;
+}
+//@}
