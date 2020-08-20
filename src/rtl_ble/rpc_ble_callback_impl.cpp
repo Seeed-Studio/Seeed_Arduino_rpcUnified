bf5fe78d644f69d6535245510d3075e8e471e3d5
LynnL4 <lht856@foxmail.com>
2020-08-20 19:05:45 +0800

    reinterpret gap callback data

--- src/rtl_ble/rpc_ble_callback_impl.cpp
+++ src/rtl_ble/rpc_ble_callback_impl.cpp
@@ -14,7 +14,7 @@ void rpc_ble_handle_gap_msg(const binary_t *gap_msg)
     // Serial.printf("\n\r");
 
     // ble_handle_gap_msg((T_IO_MSG *)gap_msg->data);
-    if(_handle_gap_msg)
+    if (_handle_gap_msg)
     {
         _handle_gap_msg((T_IO_MSG *)gap_msg->data);
     }
@@ -25,26 +25,27 @@ RPC_T_APP_RESULT rpc_ble_gap_callback(uint8_t cb_type, const binary_t *cb_data)
     RPC_T_APP_RESULT result = RPC_APP_RESULT_SUCCESS;
 
     T_LE_CB_DATA *p_cb_data = (T_LE_CB_DATA *)malloc(sizeof(T_LE_CB_DATA));
+
     switch (cb_type)
     {
-    case GAP_MSG_LE_DATA_LEN_CHANGE_INFO:
-    {
-        break;
-    }
     case GAP_MSG_LE_MODIFY_WHITE_LIST:
     {
+        p_cb_data->p_le_modify_white_list_rsp = (T_LE_MODIFY_WHITE_LIST_RSP *)cb_data->data;
         break;
     }
     case GAP_MSG_LE_CONN_UPDATE_IND:
     {
+        p_cb_data->p_le_conn_update_ind = (T_LE_CONN_UPDATE_IND *)cb_data->data;
         break;
     }
     case GAP_MSG_LE_PHY_UPDATE_INFO:
     {
+        p_cb_data->p_le_phy_update_info = (T_LE_PHY_UPDATE_INFO *)cb_data->data;
         break;
     }
     case GAP_MSG_LE_REMOTE_FEATS_INFO:
     {
+        p_cb_data->p_le_remote_feats_info = (T_LE_REMOTE_FEATS_INFO *)cb_data->data;
         break;
     }
     case GAP_MSG_LE_SCAN_INFO:
@@ -52,10 +53,47 @@ RPC_T_APP_RESULT rpc_ble_gap_callback(uint8_t cb_type, const binary_t *cb_data)
         p_cb_data->p_le_scan_info = (T_LE_SCAN_INFO *)cb_data->data;
         break;
     }
-    default:
+    case GAP_MSG_LE_ADV_UPDATE_PARAM:
+    {
+        p_cb_data->p_le_adv_update_param_rsp = (T_LE_ADV_UPDATE_PARAM_RSP *)cb_data->data;
         break;
     }
-   
+    case GAP_MSG_LE_CREATE_CONN_IND:
+    {
+        p_cb_data->p_le_create_conn_ind = (T_LE_CREATE_CONN_IND *)cb_data->data;
+        break;
+    }
+    case GAP_MSG_LE_SET_RAND_ADDR:
+    {
+        p_cb_data->p_le_set_rand_addr_rsp = (T_LE_SET_RAND_ADDR_RSP *)cb_data->data;
+        break;
+    }
+    case GAP_MSG_LE_SET_HOST_CHANN_CLASSIF:
+    {
+        p_cb_data->p_le_set_host_chann_classif_rsp = (T_LE_SET_HOST_CHANN_CLASSIF_RSP *)cb_data->data;
+        break;
+    }
+    case GAP_MSG_LE_READ_RSSI:
+    {
+        p_cb_data->p_le_read_rssi_rsp = (T_LE_READ_RSSI_RSP *)cb_data->data;
+        break;
+    }
+    case GAP_MSG_LE_SET_DATA_LEN:
+    {
+        p_cb_data->p_le_set_data_len_rsp = (T_LE_SET_DATA_LEN_RSP *)cb_data->data;
+        break;
+    }
+    case GAP_MSG_LE_DATA_LEN_CHANGE_INFO:
+    {
+        p_cb_data->p_le_data_len_change_info = (T_LE_DATA_LEN_CHANGE_INFO *)cb_data->data;
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+
     if (_ble_gap_callback != NULL)
         result = (RPC_T_APP_RESULT)_ble_gap_callback(cb_type, p_cb_data);
 
