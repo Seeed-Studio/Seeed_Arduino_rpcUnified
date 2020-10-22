97a1cd35e096ba07355b23850cd9875adc24bd9c
LynnL4 <lht856@foxmail.com>
2020-10-22 17:39:52 +0800

    add class EUart for erpc uart

--- src/erpc/erpc_arduino_uart_transport.h
+++ src/erpc/erpc_arduino_uart_transport.h
@@ -24,8 +24,56 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 namespace erpc {
+
+
+#include "HardwareSerial.h"
+#include "SERCOM.h"
+#include "RingBuffer.h"
+
+#include <cstddef>
+
+class EUart : public HardwareSerial
+{
+  public:
+    EUart(SERCOM *_s, uint8_t _pinRX, uint8_t _pinTX, SercomRXPad _padRX, SercomUartTXPad _padTX);
+    EUart(SERCOM *_s, uint8_t _pinRX, uint8_t _pinTX, SercomRXPad _padRX, SercomUartTXPad _padTX, uint8_t _pinRTS, uint8_t _pinCTS);
+    void begin(unsigned long baudRate);
+    void begin(unsigned long baudrate, uint16_t config);
+    void end();
+    int available();
+    int availableForWrite();
+    int peek();
+    int read();
+    void flush();
+    size_t write(const uint8_t data);
+    using Print::write; // pull in write(str) and write(buf, size) from Print
+
+    void IrqHandler();
+
+    operator bool() { return true; }
+
+  private:
+    SERCOM *sercom;
+    RingBufferN<4096> rxBuffer;
+    RingBuffer txBuffer;
+
+    uint8_t uc_pinRX;
+    uint8_t uc_pinTX;
+    SercomRXPad uc_padRX;
+    SercomUartTXPad uc_padTX;
+    uint8_t uc_pinRTS;
+    volatile uint32_t* pul_outsetRTS;
+    volatile uint32_t* pul_outclrRTS;
+    uint32_t ul_pinMaskRTS;
+    uint8_t uc_pinCTS;
+
+    SercomNumberStopBit extractNbStopBit(uint16_t config);
+    SercomUartCharSize extractCharSize(uint16_t config);
+    SercomParityMode extractParity(uint16_t config);
+};
+
 /*!
- * @brief Very basic transport to send/receive messages via UART.
+ * @brief Very basic transport to send/receive messages via EUart.
  *
  * @ingroup uart_transport
  */
@@ -35,7 +83,7 @@ public:
     /*!
      * @brief Constructor.
      *
-     * @param[in] uartDrv Cmsis uart.
+     * @param[in] uartDrv Cmsis EUart.
      */
     UartTransport(HardwareSerial *uartDrv, unsigned long baudrate);
 
@@ -45,11 +93,11 @@ public:
     virtual ~UartTransport(void);
 
     /*!
-     * @brief Initialize CMSIS UART peripheral configuration structure with values specified in UartTransport
+     * @brief Initialize CMSIS EUart peripheral configuration structure with values specified in UartTransport
      * constructor.
      *
-     * @retval kErpcStatus_InitFailed When UART init function failed.
-     * @retval kErpcStatus_Success When UART init function was executed successfully.
+     * @retval kErpcStatus_InitFailed When EUart init function failed.
+     * @retval kErpcStatus_Success When EUart init function was executed successfully.
      */
     virtual erpc_status_t init(void);
 
@@ -57,22 +105,22 @@ public:
 
 protected:
     HardwareSerial *m_uartDrv; /*!< Access structure of the USART Driver */
-    unsigned long m_baudrate;  /*!< Uart baud rate*/
+    unsigned long m_baudrate;  /*!< EUart baud rate*/
 
 private:
     /*!
-     * @brief Receive data from UART peripheral.
+     * @brief Receive data from EUart peripheral.
      *
      * @param[inout] data Preallocated buffer for receiving data.
      * @param[in] size Size of data to read.
      *
-     * @retval kErpcStatus_ReceiveFailed UART failed to receive data.
+     * @retval kErpcStatus_ReceiveFailed EUart failed to receive data.
      * @retval kErpcStatus_Success Successfully received all data.
      */
     virtual erpc_status_t underlyingReceive(uint8_t *data, uint32_t size);
 
     /*!
-     * @brief Write data to UART peripheral.
+     * @brief Write data to EUart peripheral.
      *
      * @param[in] data Buffer to send.
      * @param[in] size Size of data to send.
--- src/erpc_Unifiled_init.cpp
+++ src/erpc_Unifiled_init.cpp
@@ -34,38 +34,35 @@ public:
         }
     }
 };
+// #define ble_uart Serial1
+#define PIN_BLE_SERIAL_X_RX (84ul)
+#define PIN_BLE_SERIAL_X_TX (85ul)
+#define PAD_BLE_SERIAL_X_RX (SERCOM_RX_PAD_2)
+#define PAD_BLE_SERIAL_X_TX (UART_TX_PAD_0)
+#define SERCOM_BLE_SERIAL_X sercom0
+#define INTERRUPT_HANDLER_IMPLEMENT_BLE_SERIAL_X(uart) \
+ void SERCOM0_0_Handler() \
+ { \
+  (uart).IrqHandler(); \
+ } \
+ void SERCOM0_1_Handler() \
+ { \
+  (uart).IrqHandler(); \
+ } \
+ void SERCOM0_2_Handler() \
+ { \
+  (uart).IrqHandler(); \
+ } \
+ void SERCOM0_3_Handler() \
+ { \
+  (uart).IrqHandler(); \
+ }
 
-// Uart ble_uart(&SERCOM_GPIO_SERIAL_X, PIN_GPIO_SERIAL_X_RX, PIN_GPIO_SERIAL_X_TX, PAD_GPIO_SERIAL_X_RX, PAD_GPIO_SERIAL_X_TX);
-// INTERRUPT_HANDLER_IMPLEMENT_GPIO_SERIAL_X(ble_uart)
-#define ble_uart Serial1
-// #define PIN_BLE_SERIAL_X_RX (84ul)
-// #define PIN_BLE_SERIAL_X_TX (85ul)
-// #define PAD_BLE_SERIAL_X_RX (SERCOM_RX_PAD_2)
-// #define PAD_BLE_SERIAL_X_TX (UART_TX_PAD_0)
-// #define SERCOM_BLE_SERIAL_X sercom0
-// #define INTERRUPT_HANDLER_IMPLEMENT_BLE_SERIAL_X(uart) \
-//  void SERCOM0_0_Handler() \
-//  { \
-//   (uart).IrqHandler(); \
-//  } \
-//  void SERCOM0_1_Handler() \
-//  { \
-//   (uart).IrqHandler(); \
-//  } \
-//  void SERCOM0_2_Handler() \
-//  { \
-//   (uart).IrqHandler(); \
-//  } \
-//  void SERCOM0_3_Handler() \
-//  { \
-//   (uart).IrqHandler(); \
-//  }
-
-// Uart ble_uart(&SERCOM_BLE_SERIAL_X, PIN_BLE_SERIAL_X_RX, PIN_BLE_SERIAL_X_TX, PAD_BLE_SERIAL_X_RX, PAD_BLE_SERIAL_X_TX);
-// extern "C"
-// {
-// INTERRUPT_HANDLER_IMPLEMENT_BLE_SERIAL_X(ble_uart)
-// }
+EUart ble_uart(&SERCOM_BLE_SERIAL_X, PIN_BLE_SERIAL_X_RX, PIN_BLE_SERIAL_X_TX, PAD_BLE_SERIAL_X_RX, PAD_BLE_SERIAL_X_TX);
+extern "C"
+{
+INTERRUPT_HANDLER_IMPLEMENT_BLE_SERIAL_X(ble_uart)
+}
 
 UartTransport g_transport(&ble_uart, 1843200);
 MyMessageBufferFactory g_msgFactory;
@@ -90,12 +87,12 @@ void add_services(erpc::SimpleServer *server)
 
 void runClient(void *arg)
 {
+    delay(100);
    _real_body();
 }
 
 void runServer(void *arg)
 {
-    Serial.println("start erpc server");
     /* run server */
     while (true)
     {
@@ -105,11 +102,10 @@ void runServer(void *arg)
 }
 
 Thread serverThread(&runServer, configMAX_PRIORITIES,    4096, "runServer");
-Thread clientThread(&runClient, configMAX_PRIORITIES - 2, 10240, "runClient");
+Thread clientThread(&runClient, configMAX_PRIORITIES - 2, 20480, "runClient");
 
 void erpc_init()
 {
-    Serial.printf("erpc init\n\r");
     pinMode(RTL8720D_CHIP_PU, OUTPUT);
     digitalWrite(RTL8720D_CHIP_PU, LOW);
     delay(100);
@@ -142,7 +138,6 @@ void erpc_init()
 void _wrap_body()
 {
     Serial.begin(115200);
-
     erpc_init();
     
     vTaskStartScheduler();
