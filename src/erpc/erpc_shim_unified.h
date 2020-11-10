1e258ff6a88032af7116f6714447334e6f259ef8
LynnL4 <lht856@foxmail.com>
2020-11-10 09:33:28 +0800

    add system api

--- src/erpc/erpc_shim_unified.h
+++ src/erpc/erpc_shim_unified.h
@@ -1,6 +1,7 @@
 #ifndef ERPC_SHIM_UNIFIE_H
 #define ERPC_SHIM_UNIFIE_H
 #include "rpc_system_header.h"
+#include "rpc_system.h"
 #include "rpc_ble_api.h"
 #include "rpc_ble_callback.h"
 #include "rpc_ble_callback_server.h"
